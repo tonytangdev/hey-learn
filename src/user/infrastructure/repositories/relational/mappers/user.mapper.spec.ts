04dd9bc02ce220bd72305bd8d7cbe952b92d3a9f
tonytangdev <tony.tang.developer@gmail.com>
2025-02-10 17:02:28 +0100

    tests(user-mapper): add basic tests

---
+++ src/user/infrastructure/repositories/relational/mappers/user.mapper.spec.ts
@@ -0,0 +1,46 @@
+import { Email } from '../../../../../user/domain/value-objects/email.value-object';
+import { User } from '../../../../../user/domain/entities/user.entity';
+import { User as UserRelationalEntity } from '../entities/user.relational-entity';
+import { faker } from '@faker-js/faker/';
+import { UserMapper } from './user.mapper';
+import { randomUUID } from 'node:crypto';
+
+describe.only('UserMapper', () => {
+  it.each([
+    new User(new Email(faker.internet.email())),
+    new User(
+      new Email(faker.internet.email()),
+      randomUUID(),
+      faker.date.past(),
+      faker.date.past(),
+      faker.date.past(),
+    ),
+  ])('should return an entity to persist', (user) => {
+    const userRelationalEntity = UserMapper.toPersistence(user);
+    expect(userRelationalEntity).toBeInstanceOf(UserRelationalEntity);
+    expect(userRelationalEntity.id).toBe(user.id);
+    expect(userRelationalEntity.email).toBe(user.getEmail());
+    expect(userRelationalEntity.createdAt).toBe(user.createdAt);
+    expect(userRelationalEntity.updatedAt).toBe(user.updatedAt);
+    expect(userRelationalEntity.deletedAt).toBe(user.deletedAt ?? null);
+  });
+
+  it.each([null, faker.date.past()])(
+    'should return an entity to domain',
+    (date) => {
+      const userRelationalEntity = new UserRelationalEntity();
+      userRelationalEntity.id = randomUUID();
+      userRelationalEntity.email = faker.internet.email();
+      userRelationalEntity.createdAt = faker.date.past();
+      userRelationalEntity.updatedAt = faker.date.past();
+      userRelationalEntity.deletedAt = date;
+      const user = UserMapper.toDomain(userRelationalEntity);
+      expect(user).toBeInstanceOf(User);
+      expect(user.id).toBe(userRelationalEntity.id);
+      expect(user.getEmail()).toBe(userRelationalEntity.email);
+      expect(user.createdAt).toBe(userRelationalEntity.createdAt);
+      expect(user.updatedAt).toBe(userRelationalEntity.updatedAt);
+      expect(user.deletedAt).toBe(date ?? undefined);
+    },
+  );
+});
---
+++ src/user/infrastructure/repositories/relational/mappers/user.mapper.ts
@@ -0,0 +1,26 @@
+import { Email } from '../../../../../user/domain/value-objects/email.value-object';
+import { User } from '../../../../../user/domain/entities/user.entity';
+import { User as UserRelationalEntity } from '../entities/user.relational-entity';
+
+export class UserMapper {
+  static toDomain(user: UserRelationalEntity) {
+    const email = new Email(user.email);
+    return new User(
+      email,
+      user.id,
+      user.createdAt,
+      user.updatedAt,
+      user.deletedAt ?? undefined,
+    );
+  }
+
+  static toPersistence(user: User) {
+    const userRelationalEntity = new UserRelationalEntity();
+    userRelationalEntity.id = user.id;
+    userRelationalEntity.email = user.getEmail();
+    userRelationalEntity.createdAt = user.createdAt;
+    userRelationalEntity.updatedAt = user.updatedAt;
+    userRelationalEntity.deletedAt = user.deletedAt ?? null;
+    return userRelationalEntity;
+  }
+}
