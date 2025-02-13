1937e9b3818a8049931543603d0670522664d954
tonytangdev <tony.tang.developer@gmail.com>
2025-02-13 15:42:18 +0100

    feat: question entity builder

---
+++ src/quiz/domain/entity-builders/question.entity-builder.spec.ts
@@ -0,0 +1,127 @@
+import { randomUUID } from 'node:crypto';
+import { QuestionEntityBuilder } from './question.entity-builder';
+import { faker } from '@faker-js/faker';
+import { MissingQuestionValueError } from '../errors/missing-question-value.error';
+import { MissingAnswerError } from '../errors/missing-answer-value.error';
+import { MissingQuestionPropositionsError } from '../errors/missing-question-propositions.error';
+
+describe('Question Entity Builder', () => {
+  it.each([
+    {
+      id: randomUUID(),
+      answer: faker.lorem.sentence(),
+      category: faker.lorem.word(),
+      questionText: faker.lorem.sentence(),
+      organizationId: randomUUID(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: new Date(),
+      wrongAnswers: [faker.lorem.sentence(), faker.lorem.sentence()],
+    },
+    {
+      id: undefined,
+      answer: faker.lorem.sentence(),
+      category: faker.lorem.word(),
+      questionText: faker.lorem.sentence(),
+      organizationId: randomUUID(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.past(),
+      deletedAt: faker.date.past(),
+      wrongAnswers: [faker.lorem.sentence()],
+    },
+    {
+      id: randomUUID(),
+      answer: faker.lorem.sentence(),
+      category: undefined,
+      questionText: faker.lorem.sentence(),
+      organizationId: randomUUID(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.past(),
+      deletedAt: undefined,
+      wrongAnswers: [faker.lorem.sentence()],
+    },
+  ])(
+    'should build a question entity',
+    ({
+      id,
+      answer,
+      category,
+      questionText,
+      organizationId,
+      createdAt,
+      updatedAt,
+      deletedAt,
+      wrongAnswers,
+    }) => {
+      const questionBuilder = new QuestionEntityBuilder();
+
+      if (id) {
+        questionBuilder.withId(id);
+      }
+
+      if (category) {
+        questionBuilder.withCategory(category);
+      }
+
+      if (deletedAt) {
+        questionBuilder.withDeletedAt(deletedAt);
+      }
+
+      wrongAnswers.forEach((wrongAnswer) => {
+        questionBuilder.addWrongAnswer(wrongAnswer);
+      });
+
+      questionBuilder
+        .withAnswer(answer)
+        .withQuestion(questionText)
+        .withOrganizationId(organizationId)
+        .withCreatedAt(createdAt)
+        .withUpdatedAt(updatedAt);
+
+      const question = questionBuilder.build();
+
+      expect(question.id).toBeTruthy();
+      if (id) {
+        expect(question.id).toBe(id);
+      }
+      expect(question.answer.value).toBe(answer);
+      expect(question.category).toBe(category);
+      expect(question.value).toBe(questionText);
+      expect(question.organization.id).toBe(organizationId);
+      expect(question.createdAt).toBe(createdAt);
+      expect(question.updatedAt).toBe(updatedAt);
+      expect(question.deletedAt).toBe(deletedAt);
+      expect(question.propositions.length).toBe(wrongAnswers.length + 1);
+    },
+  );
+
+  it('should throw an error if the question is empty', () => {
+    const questionBuilder = new QuestionEntityBuilder()
+      .withOrganizationId(randomUUID())
+      .withQuestion('     ')
+      .withAnswer('answer');
+
+    expect(() => questionBuilder.build()).toThrow(
+      new MissingQuestionValueError(),
+    );
+  });
+
+  it('should throw an error if the answer is empty', () => {
+    const questionBuilder = new QuestionEntityBuilder()
+      .withOrganizationId(randomUUID())
+      .withQuestion('question')
+      .withAnswer('     ');
+    expect(() => questionBuilder.build()).toThrow(new MissingAnswerError());
+  });
+
+  it('should an error if the number of propositions is less than 2', () => {
+    const questionBuilder = new QuestionEntityBuilder()
+      .withOrganizationId(randomUUID())
+      .withQuestion(faker.lorem.sentence())
+      .withAnswer(faker.lorem.sentence());
+
+    expect(() => questionBuilder.build()).toThrow(
+      new MissingQuestionPropositionsError(),
+    );
+  });
+});
---
+++ src/quiz/domain/entity-builders/question.entity-builder.ts
@@ -0,0 +1,82 @@
+import { Answer } from '../entities/answer.entity';
+import { Organization } from '../entities/organization.entity';
+import { Question } from '../entities/question.entity';
+
+export class QuestionEntityBuilder {
+  private id: string;
+  private createdAt: Date;
+  private updatedAt: Date;
+  private deletedAt?: Date;
+  private value: string;
+  private organizationId: string;
+  private answer: string;
+  private wrongAnswers: string[] = [];
+  private category?: string;
+
+  withId(id: string) {
+    this.id = id;
+    return this;
+  }
+
+  withCreatedAt(createdAt: Date) {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  withUpdatedAt(updatedAt: Date) {
+    this.updatedAt = updatedAt;
+    return this;
+  }
+
+  withDeletedAt(deletedAt: Date) {
+    this.deletedAt = deletedAt;
+    return this;
+  }
+
+  withQuestion(value: string) {
+    this.value = value;
+    return this;
+  }
+
+  withOrganizationId(organizationId: string) {
+    this.organizationId = organizationId;
+    return this;
+  }
+
+  withAnswer(answer: string) {
+    this.answer = answer;
+    return this;
+  }
+
+  addWrongAnswer(wrongAnswer: string) {
+    this.wrongAnswers.push(wrongAnswer);
+    return this;
+  }
+
+  withCategory(category: string) {
+    this.category = category;
+    return this;
+  }
+
+  build() {
+    const organization = new Organization(this.organizationId);
+    const propositions = this.wrongAnswers.map(
+      (wrongAnswer) => new Answer(wrongAnswer),
+    );
+    const answer = new Answer(this.answer);
+    propositions.push(answer);
+
+    const question = new Question(
+      this.value,
+      organization,
+      answer,
+      propositions,
+      this.category,
+      this.id,
+      this.createdAt,
+      this.updatedAt,
+      this.deletedAt,
+    );
+    return question;
+  }
+}
