c0b0e0f4c5ab8807a9a91ecd1e0437e2d12c32f0
tonytangdev <tony.tang.developer@gmail.com>
2025-02-15 15:06:13 +0100

    test(quiz): add unit tests for OrganizationMembershipRelationalRepository methods

--- src/quiz/infrastructure/infrastructure/repositories/relational/repositories/organization-membership.relational.repository.spec.ts
+++ src/quiz/infrastructure/infrastructure/repositories/relational/repositories/organization-membership.relational.repository.spec.ts
@@ -1,4 +1,73 @@
+import { Repository } from 'typeorm';
+import { OrganizationMembershipRelationalRepository } from './organization-membership.relational.repository';
+import { OrganizationMembershipRelationalEntity } from '../entities/organization-membership.relational-entity';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { randomUUID } from 'node:crypto';
+
 describe('Organization Membership Relational Repository', () => {
-  it.todo('should be defined');
-  it.todo('return a membership');
+  let organizationMembershipRelationalRepository: OrganizationMembershipRelationalRepository;
+  let mockRepository: Repository<OrganizationMembershipRelationalEntity>;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        OrganizationMembershipRelationalRepository,
+        {
+          provide: getRepositoryToken(OrganizationMembershipRelationalEntity),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    organizationMembershipRelationalRepository =
+      moduleRef.get<OrganizationMembershipRelationalRepository>(
+        OrganizationMembershipRelationalRepository,
+      );
+    mockRepository = moduleRef.get<
+      Repository<OrganizationMembershipRelationalEntity>
+    >(getRepositoryToken(OrganizationMembershipRelationalEntity));
+  });
+
+  it('should be defined', () => {
+    expect(organizationMembershipRelationalRepository).toBeDefined();
+  });
+
+  it('return a membership', async () => {
+    const userId = randomUUID();
+    const organizationId = randomUUID();
+
+    const mockMembership = new OrganizationMembershipRelationalEntity();
+    mockMembership.userId = userId;
+    mockMembership.organizationId = organizationId;
+    jest.spyOn(mockRepository, 'findOne').mockResolvedValue(mockMembership);
+
+    const membership =
+      await organizationMembershipRelationalRepository.findByOrganizationIdAndUserId(
+        organizationId,
+        userId,
+      );
+
+    if (!membership) {
+      fail('Membership not found');
+    }
+
+    expect(membership).toBeDefined();
+    expect(membership.userId).toBe(userId);
+    expect(membership.organizationId).toBe(organizationId);
+  });
+
+  it('should return null if membership does not exist', async () => {
+    const userId = randomUUID();
+    const organizationId = randomUUID();
+    jest.spyOn(mockRepository, 'findOne').mockResolvedValue(null);
+
+    const membership =
+      await organizationMembershipRelationalRepository.findByOrganizationIdAndUserId(
+        organizationId,
+        userId,
+      );
+
+    expect(membership).toBeNull();
+  });
 });
