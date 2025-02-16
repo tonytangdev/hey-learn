7e197586039681377e846bdc26c9e6ff32e00104
tonytangdev <tony.tang.developer@gmail.com>
2025-02-16 13:32:28 +0100

    chore: add Docker Compose files for development and testing environments, update .gitignore for test environment variables

--- test/init-app.ts
+++ test/init-app.ts
@@ -2,11 +2,20 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { App } from 'supertest/types';
+import { ConfigModule } from '@nestjs/config';
 
 export async function initApp(app: INestApplication<App>) {
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
-  }).compile();
+  })
+    .overrideModule(ConfigModule)
+    .useModule(
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env.test',
+      }),
+    )
+    .compile();
 
   app = moduleFixture.createNestApplication();
   app.useGlobalPipes(new ValidationPipe());
