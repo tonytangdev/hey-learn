24d8138b190e04093878ccf8a555d7d6edbecf1b
tonytangdev <tony.tang.developer@gmail.com>
2025-02-16 11:16:32 +0100

    test(quiz): add end-to-end tests for quiz creation and validation

---
+++ test/quiz.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { App } from 'supertest/types';
+import { initApp } from './init-app';
+import { CreateQuizDTO } from 'src/quiz/application/dtos/create-quiz.dto';
+
+describe('QuizController (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    app = await initApp(app);
+  });
+
+  it('/quiz (POST) - create quiz', () => {
+    const payload: CreateQuizDTO = {
+      answer: 'answer',
+      question: 'question',
+      organizationId: 'organizationId',
+      userId: 'userId',
+      wrongAnswers: ['wrongAnswer1', 'wrongAnswer2'],
+      category: 'category',
+    };
+
+    return request(app.getHttpServer())
+      .post('/quiz')
+      .send(payload)
+      .expect(HttpStatus.ACCEPTED);
+  });
+
+  it('/quiz (POST) - bad request', () => {
+    const payload = {};
+    return request(app.getHttpServer())
+      .post('/quiz')
+      .send(payload)
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+});
