3c6bf35b6d0374cf42cdb931837552cf14b0e8e3
tonytangdev <tony.tang.developer@gmail.com>
2025-02-25 15:16:27 +0100

    feat(app): enable CORS with dynamic origin and update default port to 4000

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'dev'
+        ? 'http://localhost:3000'
+        : 'https://hey-learn.vercel.app',
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
